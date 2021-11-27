c5ac704768cef60993cb77955a9a14a537a67231
Maki Aboabida <maki-miko@hotmail.com>
2021-11-27 01:56:49 -0500

    Added: Added: Transaction actions are now possible offline

---
+++ public/js/idb.js
@@ -0,0 +1,69 @@
+let db;
+
+const request = indexedDB.open('budget', 1)
+
+request.onupgradeneeded = event => {
+    const db = event.target.result
+    console.log(db)
+    db.createObjectStore('new_transaction', { autoIncrement: true })
+}
+
+request.onsuccess = event => {
+    db = event.target.result 
+
+    if (navigator.onLine) {
+        uploadTransaction()
+    }
+}
+
+request.onerror = event => {
+    console.log(event.target.errorCode)
+}
+
+saveRecord = record => {
+    const transaction = db.transaction(['new_transaction'], 'readwrite')
+
+    const budgetObjectStore = transaction.objectStore('new_transaction')
+
+    budgetObjectStore.add(record)
+}
+
+uploadTransaction = () => {
+    const transaction = db.transaction(['new_transaction'], 'readwrite')
+
+    const budgetObjectStore = transaction.objectStore('new_transaction')
+
+    const getAll = budgetObjectStore.getAll()
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse)
+                }
+
+                const transaction = db.transaction(['new_transaction'], 'readwrite')
+
+                const budgetObjectStore = transaction.objectStore('new_transaction')
+
+                budgetObjectStore.clear()
+
+                alert('All saved transactions has been saved to database!')
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+    }
+}
+
+window.addEventListener('online', uploadTransaction)
